b784485c05740db5c7bb8cafbbc0514fc9ae69df
Arsh Saxena <arsh.saxena02@gmail.com>
2025-06-19 02:20:03 +0530

    fixed

--- src/components/Layout/Navbar/NavbarLinks.js
+++ src/components/Layout/Navbar/NavbarLinks.js
@@ -6,7 +6,7 @@ export default function NavbarLink({ href, icon: Icon, label, onClick }) {
 	return (
 		<Link href={href} onClick={onClick}>
 			<div className="flex w-fit items-center rounded-[25px] align-middle hover:text-[var(--accent)] md:px-3 md:py-1 md:hover:bg-[var(--main)] md:hover:text-[#f5f5f7]">
-				<Icon className="mr-4 w-[15px] pb-[1px] md:mr-1" />
+				<Icon className="mr-4 w-[15px] pb-[1px] md:mr-1.5" />
 				{label}
 			</div>
 		</Link>
